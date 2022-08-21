9cbbd772956fb6de0e9f2ec49a9d06d9a81a34c5
kom50 <omprakashkartik82280@gmail.com>
2022-08-21 14:55:09 +0530

    Add PWA feature 1

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,53 @@
 let cacheData = 'app-v1'
 
+const caching_files = [
+    './static/js/bundle.js',
+    './static/js/vendors~main.chunk.js',
+    './static/js/main.chunk.js',
+    './static/media/om.ba8dc23e.jpg',
+    './index.html',
+    './web-om/#/',
+    './favicon.ico',
+    './manifest.json',
+    './img/icons/om-32x32.png',
+    './img/icons/om-192x192.png',
+    './img/icons/om-512x512.png',
+    '.https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;1,300&display=swap',
+    './',
+]
+
 this.addEventListener('install', (event) => {
-    console.log("🚀 ~ file: sw.js ~ line 4 ~ this.addEventListener ~ event", event)
+    console.log("Service Worker : Installed!")
     event.waitUntil(
-        caches.open(cacheData).then((cache) => {
-            cache.addAll([
-                '/static/js/bundle.js',
-                '/static/js/vendors~main.chunk.js',
-                '/static/js/main.chunk.js',
-                '/static/media/om.ba8dc23e.jpg',
-                '/index.html',
-                '/favicon.ico',
-                'https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;1,300&display=swap',
-                '/',
-            ])
-        })
+        (async () => {
+            try {
+                const cache_obj = await caches.open(cacheData)
+                cache_obj.addAll(caching_files)
+            }
+            catch {
+                console.log("error occurred while caching...")
+            }
+        })()
     )
+    // event.waitUntil(
+    //     caches.open(cacheData).then((cache) => {
+    //         cache.addAll([
+    //             '/static/js/bundle.js',
+    //             '/static/js/vendors~main.chunk.js',
+    //             '/static/js/main.chunk.js',
+    //             '/static/media/om.ba8dc23e.jpg',
+    //             '/index.html',
+    //             '/favicon.ico',
+    //             '/web-om/#/',
+    //             '/manifest.json',
+    //             '/img/icons/om-32x32.png',
+    //             '/img/icons/om-192x192.png',
+    //             '/img/icons/om-512x512.png',
+    //             'https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;1,300&display=swap',
+    //             '/',
+    //         ])
+    //     })
+    // )
 })
 
 this.addEventListener('fetch', (event) => {
@@ -23,8 +55,7 @@ this.addEventListener('fetch', (event) => {
     if (!navigator.onLine) {
         event.respondWith(
             caches.match(event.request).then(response => {
-                if (response)
-                    return response
+                return response || fetch(event.request)
             })
         )
     }
